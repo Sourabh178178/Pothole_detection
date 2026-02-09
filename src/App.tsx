5cb9e51e0192c09c37f845c5d4b29c7e2f2f0d8f
Sourabh178178 <sourabhbettad178@gmail.com>
2026-02-09 16:12:35 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import { Layout } from "@/components/Layout";
+import Home from "./pages/Home";
+import Detect from "./pages/Detect";
+import Results from "./pages/Results";
+import Model from "./pages/Model";
+import NotFound from "./pages/NotFound";
+import Contributions from "./pages/Contributions";
+
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/detect" element={<Detect />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="/model" element={<Model />} />
+              <Route path="/contributions" element={<Contributions />} />
+
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
+
+export default App;
